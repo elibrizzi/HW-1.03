19c00d7e2f18e7fc6540dd30bb7e4192b612f60a
elibrizzi <145513762+elibrizzi@users.noreply.github.com>
2023-09-20 13:28:17 -0500

    why isnt the image loading

---
+++ Help/sketch.js
@@ -0,0 +1,13 @@
+var rodion;
+
+function preload(){
+  rodion = createImg("https://static.wikia.nocookie.net/limbuscompany/images/1/1c/Rodyafull.jpg/revision/latest?cb=20230107143805");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+
+rodion.position(0,0);
+
